3317f63ac69548a88f3e51ca649e5577bff6df07
plasticbox <plasticbox@gmail.com>
2023-04-15 10:46:12 +0900

    startup

--- rust_bevy/src/main.rs
+++ rust_bevy/src/main.rs
@@ -1,13 +1,12 @@
-use bevy::prelude::*;
-use bevy::hierarchy::Children;
 use bevy::hierarchy::BuildChildren;
+use bevy::hierarchy::Children;
+use bevy::prelude::*;
 
 #[derive(StageLabel)]
 pub(crate) enum Stage {
     AStage,
     BStage,
     CStage,
-    DStage,
 }
 
 #[derive(Component, Debug)]
@@ -22,27 +21,27 @@ struct TestChildB(String);
 #[derive(Component, Debug)]
 struct TestChildC;
 
-
-fn spawn(mut cmds: Commands, mut is_run: Local<bool>) {
-    if !*is_run {
-        println!("spawn!");
-        cmds.spawn(TestParent)
-            .with_children(|parent| {
-                parent.spawn(TestChildA {0: 1});
-                parent.spawn(TestChildB {0: "test".to_string()});
-            });
-
-        *is_run = true;
-    }
+fn spawn(mut cmds: Commands) {
+    println!("spawn!");
+    cmds.spawn(TestParent).with_children(|parent| {
+        parent.spawn(TestChildA { 0: 1 });
+        parent.spawn(TestChildB {
+            0: "test".to_string(),
+        });
+    });
 }
 
-fn add_detect_and_remove(mut cmds: Commands, query: Query<(Entity, &TestParent, &Children), Added<Children>>, a_query: Query<&TestChildA>) {
+fn added_detect_and_remove(
+    mut cmds: Commands,
+    query: Query<(Entity, &TestParent, &Children), Added<Children>>,
+    a_query: Query<&TestChildA>,
+) {
     query.iter().for_each(|(entity, parent, children)| {
-        println!("add_detect parent: {:?}", parent);
-        println!("add_detect children: {:?}", children);
+        println!("added_detect parent: {:?}", parent);
+        println!("added_detect children: {:?}", children);
         children.iter().for_each(|child_entity| {
-            println!("add_detect child: {:?}", child_entity);
             if a_query.get(*child_entity).ok().is_some() {
+                println!("remove child: {:?}", child_entity);
                 cmds.entity(entity).remove_children(&[*child_entity]);
                 cmds.entity(*child_entity).despawn();
             }
@@ -54,39 +53,43 @@ fn change_detect(
     query: Query<(&TestParent, &Children, ChangeTrackers<Children>), Changed<Children>>,
 ) {
     query.iter().for_each(|(parent, children, tracker)| {
-        if tracker.is_added() {
-            return;
+        if tracker.is_changed() {
+            println!("change_detect parent: {:?}", parent);
+            println!("change_detect children: {:?}", children);
         }
-
-        println!("change_detect parent: {:?}", parent);
-        println!("change_detect children: {:?}", children);
     });
 }
 
 fn add_child(
     mut cmds: Commands,
     query: Query<(Entity, &TestParent, &Children)>,
+    mut local: Local<bool>,
 ) {
+    if *local {
+        return;
+    }
+
+    *local = true;
     query.iter().for_each(|(entity, _, _)| {
         let id = cmds.spawn(TestChildC).id();
         cmds.entity(entity).add_child(id);
 
-        println!("add_child");
+        println!("add_child {:?}", id);
     });
 }
 
 fn main() {
-
     let mut app = App::new();
 
     app.add_stage(Stage::AStage, SystemStage::parallel());
     app.add_stage(Stage::BStage, SystemStage::parallel());
     app.add_stage(Stage::CStage, SystemStage::parallel());
-    app.add_stage(Stage::DStage, SystemStage::parallel());
-    app.add_system_to_stage(Stage::AStage, spawn);
-    app.add_system_to_stage(Stage::BStage, add_detect_and_remove);
-    app.add_system_to_stage(Stage::CStage, change_detect);
-    app.add_system_to_stage(Stage::DStage, add_child);
+
+    app.add_startup_system(spawn);
+
+    app.add_system_to_stage(Stage::AStage, added_detect_and_remove);
+    app.add_system_to_stage(Stage::BStage, change_detect);
+    app.add_system_to_stage(Stage::CStage, add_child);
 
     println!("update 1");
     app.update();
